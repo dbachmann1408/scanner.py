c3feec771c29bbf1c5e7046609564f00521d3f6a
Michael Korens <mkore760@gmail.com>
2018-07-07 14:39:41 -0700

    10/10 code on Pylint

--- scanner.py
+++ scanner.py
@@ -6,9 +6,12 @@ import sys    # import the sys library for System-specific parameters and functi
 
 
 def open_ports(host, port):  # Return Boolean
+    """
+    Creates a socket stream and attempts to connect to a port
+    """
     try:    # try connecting to host at port and if successful return true if false got to except
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # declare sock variable using AF_INET which sets the
-        # IPv4 and SOCK_STREAM sets protocol to TCP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # declare sock variable using AF_INET which sets the IPv4 and SOCK_STREAM to TCP
         sock.connect((host, port))  # connects the socket to the declared host and port
     except socket.error:  # throws an exception returns false
         return False
@@ -17,11 +20,12 @@ def open_ports(host, port):  # Return Boolean
     return True
 
 
-list_of_ports = [20, 21, 22, 23, 25, 53, 68, 69, 80, 110, 115,
-                 119, 123, 135, 137, 138, 139, 143, 161,
-                 194, 311, 443, 445, 660, 993, 1023]  # list which holds the ports that are going to be scanned
+# CONFIG - Lists the ports that will be scanned
+LIST_OF_PORTS = [20, 21, 22, 23, 25, 53, 68, 69, 80, 110, 115,
+                 119, 123, 135, 137, 138, 139, 143, 161, 194,
+                 311, 443, 445, 660, 993, 1023]
 
-for ports in list_of_ports:  # for loop which cycles through the ports
+for ports in LIST_OF_PORTS:  # for loop which cycles through the ports
     if open_ports(sys.argv[0], ports):
         print("Port {} is OPEN!".format(ports))  # prints the open ports to the console
     else:
